c83b9f692455b88f0638eac2c6b568d0f2908138
gitjiho <jiho99322@gmail.com>
2025-05-07 01:58:03 +0900

    test

--- src/InterviewAssistant.AppHost/Program.cs
+++ src/InterviewAssistant.AppHost/Program.cs
@@ -1,18 +1,22 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Automatically provision an Application Insights resource
+var insights = builder.AddAzureApplicationInsights("applicationinsights");
+
 var openai = builder.AddConnectionString("openai");
 
 var config = builder.Configuration;
 
 var apiService = builder.AddProject<Projects.InterviewAssistant_ApiService>("apiservice")
                     .WithReference(openai)
+                    .WithReference(insights)
                     .WithEnvironment("SemanticKernel__ServiceId", config["SemanticKernel:ServiceId"]!)
                     .WithEnvironment("GitHub__Models__ModelId", config["GitHub:Models:ModelId"]!);
-
 
 builder.AddProject<Projects.InterviewAssistant_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
+    .WithReference(insights)
     .WaitFor(apiService);
 
 builder.Build().Run();
